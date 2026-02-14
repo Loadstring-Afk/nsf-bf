3962533601e5fbc08f91a37951c95247cd42c992
Loadstring-Afk <elvismateopedro@gmail.com>
2026-02-14 02:28:48 -0600

    Update assetsConfig.js

--- src/utils/assetsConfig.js
+++ src/utils/assetsConfig.js
@@ -3,17 +3,18 @@ export const assetsConfig = {
   music: "https://drive.google.com/file/d/1hHfcN8TYU1qBu_NjmzOTp9UKCsVMCjhf/view?usp=drivesdk", // Replace with your MP3 URL
   
   noGifs: [
-    "https://tenor.com/view/sponge-squid-nothing-sad-gif-19385182",
-    "https://tenor.com/view/cumple-a%C3%B1os-gif-24770809",
-    "https://tenor.com/view/valentines-sad-charlie-brown-sad-valentine-gif-13385460",
-    "https://tenor.com/view/no-valentine-sad-valentine-ralph-simpsons-gif-13496238",
-    "https://tenor.com/view/sad-valentine-charlie-brown-gif-13495082",
-    "https://tenor.com/view/broken-valentines-day-sad-heart-vs-happy-heart-happy-heart-heals-the-broken-heart-be-real-gif-16167928",
-    "https://tenor.com/view/sad-lonely-unhappy-sigh-dia-de-san-valentin-gif-16310532",
-    
+    "https://giphy.com/gifs/saltaclysm-meme-salt-salty-qUakGPHXR1pCtQdJmH",
+    "https://giphy.com/gifs/peanuts-charlie-brown-be-my-valentine-ggqIsZm7oLbvG",
+    "https://giphy.com/gifs/Gameforge-gaming-pet-metin2-TYuolblmGWJbgz1nAL",
+    "https://giphy.com/gifs/pudgypenguins-heart-cupid-broken-fpFa9Nl3QZOVYYRM83",
+    "https://giphy.com/gifs/valentines-day-forever-alone-gKW26O7cU2JDG",
+    "https://giphy.com/gifs/pudgypenguins-heart-cupid-broken-fpFa9Nl3QZOVYYRM83",
+    "https://giphy.com/gifs/pudgypenguins-heart-cupid-broken-fpFa9Nl3QZOVYYRM83",
+    https://giphy.com/gifs/Gameforge-gaming-pet-metin2-TYuolblmGWJbgz1nAL
 "https://tenor.com/view/sad-gif-27188097",
-    "https://tenor.com/view/sad-gif-27188097",
-    "https://share.google/CNsbOTGwO4VmkI5M1"
+    "https://giphy.com/gifs/Gameforge-gaming-pet-metin2-TYuolblmGWJbgz1nAL",
+
+"https://giphy.com/gifs/saltaclysm-meme-salt-salty-qUakGPHXR1pCtQdJmH"
   ],
   
   gallery: [
